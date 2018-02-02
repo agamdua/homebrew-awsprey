f1e1a9b19b3187996948f9d90b17078f63d60e37
Agam Dua <agam@comfylabs.io>
2018-02-01 23:36:17 -0500

    awsprey version 0.2.0

--- Formula/awsprey.rb
+++ Formula/awsprey.rb
@@ -1,9 +1,9 @@
 class Awsprey < Formula
   desc ""
   homepage ""
-  url "https://github.com/agamdua/awsprey/releases/download/v0.1.2/awsprey_0.1.2_darwin_amd64.tar.gz"
-  version "0.1.2"
-  sha256 "cbd7a0afa14d825fdbc54c4c5ac73fa69caf94c49d59ec035a0a13c2ded189ce"
+  url "https://github.com/agamdua/awsprey/releases/download/0.2.0/awsprey_0.2.0_darwin_amd64.tar.gz"
+  version "0.2.0"
+  sha256 "ad2a58cf3111bdc545bb347a547c88d107eaa22af0fefd4c9cf2be3f21f3ccd6"
 
   def install
     bin.install "awsprey"
